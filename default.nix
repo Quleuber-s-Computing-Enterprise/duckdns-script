1f83b5739c5675bba70932d684846b3c37249ab3
Kelvin Steiner <me@steinerkelvin.dev>
2023-06-24 09:54:03 -0300

    replace curl call with curl from nix

--- default.nix
+++ default.nix
@@ -4,13 +4,14 @@ pkgs.stdenv.mkDerivation {
   name = "duckdns-script";
   src = ./src;
 
-  buildInputs = [ pkgs.bash ];
+  # buildInputs = [ pkgs.bash pkgs.curl ];
 
   installPhase = ''
     mkdir -p $out/bin
 
     substitute ${./src/duckdns-script} $out/bin/duckdns-script \
-      --replace "#!/usr/bin/env bash" "#!${pkgs.bash}/bin/bash"
+      --replace "#!/usr/bin/env bash" "#!${pkgs.bash}/bin/bash" \
+      --replace "curl_cmd=curl" "curl_cmd=${pkgs.curl}/bin/curl"
 
     chmod +x $out/bin/duckdns-script
   '';
